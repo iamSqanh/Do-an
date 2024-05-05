658e90b9079f7cfef2b9d9c08fe076d7788eec01
sam anh <samquocanh0303@gmail.com>
2024-05-05 21:44:03 +0700

    update

--- client/src/ultils/dataContact.js
+++ client/src/ultils/dataContact.js
@@ -1,22 +1,21 @@
 export const text = {
-    image: 'https://phongtro123.com/images/support-bg.jpg',
-    content: 'Liên hệ với chúng tôi nếu bạn cần hỗ trợ:',
-    contacts: [
-        {
-            text: 'HỖ TRỢ THANH TOÁN',
-            phone: 'Điện thoại: 0917686101',
-            zalo: 'Zalo: 0917686101'
-        },
-        {
-            text: 'HỖ TRỢ ĐĂNG TIN',
-            phone: 'Điện thoại: 0902657123',
-            zalo: 'Zalo: 0902657123'
-        },
-        {
-            text: 'HOTLINE 24/7',
-            phone: 'Điện thoại: 0917686101',
-            zalo: 'Zalo: 0917686101'
-        },
-    ]
-}
-
+  image: "https://phongtro123.com/images/support-bg.jpg",
+  content: "Liên hệ với chúng tôi nếu bạn cần hỗ trợ:",
+  contacts: [
+    {
+      text: "HỖ TRỢ THANH TOÁN",
+      phone: "Điện thoại: 0989 999 999",
+      zalo: "Zalo: 0989 999 999",
+    },
+    {
+      text: "HỖ TRỢ ĐĂNG TIN",
+      phone: "Điện thoại: 098 765 2000 ",
+      zalo: "Zalo: 098 765 2000 ",
+    },
+    {
+      text: "HOTLINE 24/7",
+      phone: "Điện thoại: 096 999 5555",
+      zalo: "Zalo: 096 999 5555",
+    },
+  ],
+};
